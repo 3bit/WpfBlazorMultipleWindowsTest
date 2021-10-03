d3d948d248fe953d7740fc167387bb39def54c84
Achim Gmeiner <achim@gmeiner.cc>
2021-10-03 21:31:59 +0200

    Initial commit

---
+++ WpfBlazorMultipleWindowsTest/Window1.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Extensions.DependencyInjection;
+using WpfBlazorMultipleWindowsTest.Blazor;
+
+namespace WpfBlazorMultipleWindowsTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public IServiceProvider ServiceProvider { get; }
+
+        public Window1()
+        {
+            var services = new ServiceCollection();
+            services.AddBlazorWebView();
+            services.AddSingleton<IWindowManager, WindowManager>();
+            ServiceProvider = services.BuildServiceProvider();
+            InitializeComponent();
+        }
+    }
+}
---
+++ WpfBlazorMultipleWindowsTest/WindowManager.cs
@@ -0,0 +1,14 @@
+﻿using WpfBlazorMultipleWindowsTest.Blazor;
+
+namespace WpfBlazorMultipleWindowsTest
+{
+    internal class WindowManager : IWindowManager
+    {
+        
+        public void OpenAnotherWindow()
+        {
+            var window = new Window1();
+            window.Show();
+        }
+    }
+}
